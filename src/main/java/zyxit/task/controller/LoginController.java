fdf977516cce46d84ef88f7e2eedd0f6d0b53ec9
Roman <matviichuk.office@gmail.com>
2020-11-04 05:17:24 +0200

    added LoginController

---
+++ src/main/java/zyxit/task/controller/LoginController.java
@@ -0,0 +1,13 @@
+package zyxit.task.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    String login() {
+        return "login";
+    }
+}
